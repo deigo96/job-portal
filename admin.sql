6898e8cb42f8ab3c1b852a9dd3493bf8bd675bb8
deigo siahaan <deigosiahaan@gmail.com>
2021-06-09 21:15:08 +0700

    change database

--- admin.sql
+++ admin.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2020 at 05:16 AM
+-- Generation Time: Jun 09, 2021 at 04:01 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.3
 
@@ -33,19 +33,16 @@ CREATE TABLE `admin` (
   `aName` varchar(255) NOT NULL,
   `aDate` datetime NOT NULL,
   `aEmail` varchar(255) NOT NULL,
-  `aPassword` varchar(255) NOT NULL
+  `aPassword` varchar(255) NOT NULL,
+  `aImage` varchar(255) NOT NULL DEFAULT 'admin.png'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`aId`, `aName`, `aDate`, `aEmail`, `aPassword`) VALUES
-(1, 'deigo', '2020-11-26 20:17:24', 'deigosiahaan@gmail.com', 'admin'),
-(2, 'ell', '2020-12-04 16:50:04', 'sitilestari@gmail.com', 'admin'),
-(3, 'agus', '2020-12-04 16:50:19', 'agusprayitno@gmail.com', 'admin'),
-(4, 'rizky', '2020-12-04 16:50:19', 'rizkymasta@gmail.com', 'admin'),
-(5, 'christin', '2020-12-04 16:51:47', 'christin@gmail.com', 'admin');
+INSERT INTO `admin` (`aId`, `aName`, `aDate`, `aEmail`, `aPassword`, `aImage`) VALUES
+(1, 'Deigo Siahaan', '2020-11-26 20:17:24', 'deigosiahaan@gmail.com', 'admin', 'deigo.jpg');
 
 -- --------------------------------------------------------
 
@@ -67,12 +64,10 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`cId`, `cName`, `cStatus`, `cDate`, `cDp`, `adminId`) VALUES
-(27, 'Samsung', 1, '2020-12-12 04:56:54', 'samsunglogo.png', 1),
-(28, 'Apple', 1, '2020-12-12 04:57:16', 'iphonelogo.png', 1),
-(29, 'Vivo', 1, '2020-12-12 04:57:35', 'vivologo.png', 1),
-(30, 'Asus', 1, '2020-12-12 04:58:02', 'asuslogo.png', 1),
-(31, 'Xiaomi', 1, '2020-12-12 04:58:25', 'xiamilogo.png', 1),
-(32, 'Huawei', 1, '2020-12-12 04:58:34', 'huaweilogo.png', 1);
+(35, 'Design', 1, '2021-05-27 09:44:46', 'staff.png', 1),
+(36, 'Marketing', 1, '2021-06-09 05:19:04', 'staff1.png', 1),
+(37, 'Operator', 1, '2021-06-09 05:19:51', 'staff2.png', 1),
+(38, 'IT', 1, '2021-06-09 05:20:12', 'staff3.png', 1);
 
 -- --------------------------------------------------------
 
@@ -84,6 +79,7 @@ CREATE TABLE `models` (
   `mId` int(11) NOT NULL,
   `mName` varchar(200) NOT NULL,
   `price` float NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
   `mDate` datetime NOT NULL,
   `mStatus` int(11) NOT NULL,
   `productId` int(11) NOT NULL,
@@ -96,15 +92,9 @@ CREATE TABLE `models` (
 -- Dumping data for table `models`
 --
 
-INSERT INTO `models` (`mId`, `mName`, `price`, `mDate`, `mStatus`, `productId`, `adminId`, `mDp`, `mDescription`) VALUES
-(14, '8 RAM', 1099.99, '2020-12-12 05:10:20', 1, 8, 1, 'sgalaxy_x1.jpg', 'Samsung Galaxy X (RAM 8 GB)'),
-(15, '32 GB', 1299.99, '2020-12-12 05:12:23', 1, 14, 1, 'iphone121.jpg', 'Iphone 12 (RAM 32 GB)'),
-(16, '32 GB', 1289.99, '2020-12-12 05:13:08', 1, 13, 1, 'iphone_X.jpg', 'Iphone X (RAM 32 GB)'),
-(17, '16 GB', 1890.99, '2020-12-12 05:13:46', 1, 9, 1, 'sfold1.jpg', 'Samsung Fold (RAM 16 GB)'),
-(18, '8 RAM', 1099.99, '2020-12-12 05:10:20', 1, 8, 1, 'sgalaxy_x1.jpg', 'Samsung Galaxy X (RAM 8 GB)'),
-(19, '32 GB', 1289.99, '2020-12-12 05:13:08', 1, 13, 1, 'iphone_X.jpg', 'Iphone X (RAM 32 GB)'),
-(20, '32 GB', 1299.99, '2020-12-12 05:12:23', 1, 14, 1, 'iphone121.jpg', 'Iphone 12 (RAM 32 GB)'),
-(21, '16 GB', 1890.99, '2020-12-12 05:13:46', 1, 9, 1, 'sfold1.jpg', 'Samsung Fold (RAM 16 GB)');
+INSERT INTO `models` (`mId`, `mName`, `price`, `location`, `mDate`, `mStatus`, `productId`, `adminId`, `mDp`, `mDescription`) VALUES
+(24, 'PT.Lazada Indonesia', 9750000, 'Cikarang', '2021-06-09 05:04:27', 1, 24, 1, 'lazada2.png', '-Mahir Desaign\r\n-Cakap dalam bekerja'),
+(25, 'PT.Lazada Indonesia', 12000000, 'Jakarta', '2021-06-09 05:44:10', 1, 27, 1, 'lazada4.png', '-Paham Mobile Progamming');
 
 -- --------------------------------------------------------
 
@@ -119,8 +109,8 @@ CREATE TABLE `products` (
   `pDate` datetime NOT NULL,
   `categoryId` int(11) NOT NULL,
   `adminId` int(11) NOT NULL,
-  `pDp` varchar(200) NOT NULL,
-  `pCompany` varchar(100) NOT NULL
+  `pDp` varchar(200) DEFAULT NULL,
+  `pCompany` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -128,13 +118,14 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`pId`, `pName`, `pStatus`, `pDate`, `categoryId`, `adminId`, `pDp`, `pCompany`) VALUES
-(8, 'Samsung Galaxy x', 1, '2020-12-12 05:00:50', 27, 1, 'sgalaxy_x.jpg', 'samsung'),
-(9, 'Samsung Galaxy Fold', 1, '2020-12-12 05:01:57', 27, 1, 'sfold.jpg', 'samsung'),
-(10, 'Iphone Xr', 1, '2020-12-12 05:03:09', 28, 1, 'iphone_xr.jpg', 'apple'),
-(11, 'Samsung Galaxy A8', 1, '2020-12-12 05:04:17', 27, 1, 'sa8.jpg', 'samsung'),
-(12, 'Samsung Galaxy S20', 1, '2020-12-12 05:04:40', 27, 1, 'sa201.jpg', 'samsung'),
-(13, 'Iphone X', 1, '2020-12-12 05:06:06', 28, 1, 'iphone_X.jpg', 'apple'),
-(14, 'Iphone 12', 1, '2020-12-12 05:06:21', 28, 1, 'iphone12.jpg', 'apple');
+(24, 'Desaign & Creative', 1, '2021-06-09 04:51:43', 35, 1, 'female1.png', NULL),
+(25, 'Desaign & Development', 1, '2021-06-09 05:05:24', 35, 1, 'staff.png', NULL),
+(26, 'Content Writer', 1, '2021-06-09 05:20:39', 35, 1, 'staff1.png', NULL),
+(27, 'Mobile Application', 1, '2021-06-09 05:21:03', 38, 1, 'staff2.png', NULL),
+(28, 'Sales & Marketing', 1, '2021-06-09 05:21:32', 36, 1, 'staff3.png', NULL),
+(29, 'Construction', 1, '2021-06-09 05:21:53', 37, 1, 'staff4.png', NULL),
+(30, 'Computer Engineer', 1, '2021-06-09 05:22:41', 38, 1, 'staff5.png', NULL),
+(31, 'Real Estate', 1, '2021-06-09 05:23:00', 36, 1, 'staff6.png', NULL);
 
 -- --------------------------------------------------------
 
@@ -174,22 +165,34 @@ CREATE TABLE `spec_values` (
 
 CREATE TABLE `users` (
   `uId` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `address` varchar(255) DEFAULT '-',
+  `education` varchar(200) DEFAULT '-',
+  `skills` varchar(255) DEFAULT '-',
   `password` varchar(100) NOT NULL,
-  `link` varchar(200) NOT NULL,
-  `date` datetime NOT NULL,
-  `status` int(11) NOT NULL DEFAULT 1
+  `images` varchar(200) DEFAULT 'user.png',
+  `cv` varchar(200) NOT NULL DEFAULT '-',
+  `date` date NOT NULL,
+  `aId` int(11) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`uId`, `first_name`, `last_name`, `email`, `password`, `link`, `date`, `status`) VALUES
-(1, 'jonathan', 'siahaan', 'deigosiahaan@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'asdasdasdasd', '2020-12-08 17:19:20', 0),
-(2, 'deigo', 'siahaan', 'brengsek975@gmail.com', '21232f297a57a5a743894a0e4a801fc3', 'asdasda', '2020-12-07 17:19:20', 1);
+INSERT INTO `users` (`uId`, `name`, `email`, `address`, `education`, `skills`, `password`, `images`, `cv`, `date`, `aId`) VALUES
+(3, 'deigo', 'deigosiahaan@gmail.com', 'Cikarang', 'd3 sistem informasi', 'php,js,html,css', '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', 'CV.docx', '2021-05-24', 1),
+(4, 'voker', 'voker@gmail.com', 'cikarang', 'S1 Ilmu Hukum', 'pp', '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', '', '2021-06-01', 1),
+(5, 'anti', 'antimage@gmail.com', '-', '-', '-', '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', '', '2021-06-06', 1),
+(6, 'tinker', 'tinker@gmail.com', '-', '-', NULL, '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', '', '2021-06-03', 1),
+(7, 'pudge', 'pudge@gmail.com', '-', '-', '-', '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', '', '2021-06-01', 1),
+(8, 'pudge', 'pudgehook@gmail.com', '-', '-', '-', '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', '', '2021-06-02', 1),
+(9, 'medusa', 'medusa@gmail.com', '-', '-', '-', '827ccb0eea8a706c4c34a16891f84e7b', 'female1.png', '', '2021-06-04', 1),
+(10, 'wind', 'wr@gmail.com', '-', '-', '-', '827ccb0eea8a706c4c34a16891f84e7b', 'user.png', '', '2021-06-07', 1),
+(12, 'fasd', 'fasdsa@gmail.com', '-', '-', '-', '0cc175b9c0f1b6a831c399e269772661', 'user.png', '', '2021-06-08', 1),
+(18, 'John F Kennedy', 'john@gmail.com', 'USA', '-', '-', '827ccb0eea8a706c4c34a16891f84e7b', 'default.png', '-', '2021-06-07', 1),
+(19, 'John Lennon', 'lennon@gmail.com', 'London', 'S1 Ilmu Hukum', 'Mampu bekerja dalam tim', '827ccb0eea8a706c4c34a16891f84e7b', 'deigo.jpg', 'CV1.docx', '2021-06-07', 1);
 
 --
 -- Indexes for dumped tables
@@ -261,19 +264,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `cId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `models`
 --
 ALTER TABLE `models`
-  MODIFY `mId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `mId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `pId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `pId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `spec_values`
@@ -285,7 +288,7 @@ ALTER TABLE `spec_values`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `uId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
