5c0e64d7b794463b2b9d7b1983c925f5ab084884
deigo siahaan <deigosiahaan@gmail.com>
2021-06-17 20:43:22 +0700

    Change Password

---
+++ application/views/admin/adminProfile.php
@@ -0,0 +1,169 @@
+
+    
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="<?php echo site_url('admin'); ?>" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-list-ul"></i>
+                        <p>Categories
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/newCategory'); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>New category</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allCategories'); ?>" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>All categories</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>Product
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/newProduct'); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>New Product</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allProducts'); ?>" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>All Products</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>Model
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/newModel'); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>New Model</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allModels'); ?>" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>All Models</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>User
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allUsers'); ?>" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>All users</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Profile</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">User Profile</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-6 offset-3">
+
+                    <!-- Profile Image -->
+                    <div class="card card-primary card-outline">
+                        <div class="card-body box-profile">
+                            <div class="text-center">
+                                <img class="profile-user-img img-fluid img-circle" src="<?php echo site_url('assets/images/profile_pictures/'). $profiles['aImage']; ?>" alt="User profile picture">
+                            </div>
+                            <h3 class="profile-username text-center"><?php echo $profiles['aName']; ?></h3>
+                            <p class="text-muted text-center"><?= $profiles['aEmail'] ?></p>
+                            <hr>
+                            <!-- <strong><i class="fas fa-book mr-1"></i> Education</strong>
+                            <p class="text-muted">
+                                <?= $users[0]['education'] ?>
+                            </p>
+                            <hr>
+                            <strong><i class="fas fa-map-marker-alt mr-1"></i> Address</strong>
+                            <p class="text-muted"><?= $users[0]['address'] ?></p>
+                            <hr>
+                            <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+                            <p class="text-muted">
+                                <?= $users[0]['skills'] ?>
+                            </p>
+                            <hr>
+                            <strong><i class="far fa-file-alt mr-1"></i> CV</strong>
+                            <p class="text-muted"><a href="<?php echo site_url('assets/images/cv/'). $users[0]['cv'] ?>"><?= $users[0]['cv'] ?></a></p> -->
+                            <a href="<?php echo base_url('admin/changePassword/').$profiles['aId'] ?>" class="btn btn-primary btn-block"><b>Change Password</b></a>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            
+            </div>
+            <!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+    
---
+++ application/views/admin/changePassword.php
@@ -0,0 +1,149 @@
+<!-- Sidebar Menu -->
+<nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+                with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="<?php echo site_url('admin'); ?>" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-list-ul"></i>
+                        <p>Categories
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/newCategory'); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>New category</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allCategories'); ?>" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>All categories</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>Product
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/newProduct'); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>New Product</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allProducts'); ?>" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>All Products</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>Model
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/newModel'); ?>" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>New Model</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allModels'); ?>" class="nav-link">
+                            <i class="far fa-circle nav-icon"></i>
+                            <p>All Models</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link active">
+                        <i class="nav-icon fas fa-layer-group"></i>
+                        <p>User
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="<?php echo site_url('admin/allUsers'); ?>" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                            <p>All users</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside><!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <section class="content">
+            <div class="row">
+                <div class="col-md-6 offset-3 mt-3">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Change Password</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <?php if($this->session->flashdata('class')): ?>
+                                <div class="alert <?php echo $this->session->flashdata('class') ?> alert-dimissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true"></span></button>
+                                    <?php echo $this->session->flashdata('message'); ?>
+                                </div>
+                            <?php endif ; ?>
+                            <?php echo form_open_multipart('admin/updatePassword', '', '') ?>
+                            <div class="form-group">
+                                <input type="password" name="oldpass" id="oldpass" class="form-control" placeholder="Old Password" />
+                                <?php echo form_error('oldpass', '<div class="alert-danger">', '</div>')?>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="newpass" id="newpass" class="form-control" placeholder="New Password" />
+                                <?php echo form_error('newpass', '<div class="alert-danger">', '</div>')?>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="passconf" id="passconf" class="form-control" placeholder="Confirm Password" />
+                                <?php echo form_error('passconf', '<div class="alert-danger">', '</div>')?>
+                            </div>
+                            <?php echo form_submit('Change Password', 'Change Password', 'class="btn btn-primary offset-4"') ?>
+                        </div>
+                        <?php echo form_close(); ?>
+
+                        <!-- /.card-body -->
+                    </div>
+                <!-- /.card -->
+                </div>
+                
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
